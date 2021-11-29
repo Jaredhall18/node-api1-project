9277b7f69c48be614d8a4d2005e591b3f3de2d66
jaredhall18 <jaredhall18@gmail.com>
2021-11-29 14:46:38 -0700

    installed express and linked server to index js.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // BUILD YOUR SERVER HERE
+// IMPORTS AT THE TOP
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// INSTANCE OF EXPRESS APP
+const server = express()
+
+// GLOBAL MIDDLEWARE
+server.use(express.json())
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
