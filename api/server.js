5d5004c38f4916a5cd8db2f27b5161a271fba9ac
jaredhall18 <jaredhall18@gmail.com>
2021-11-29 16:44:21 -0700

    completed mvp

--- api/server.js
+++ api/server.js
@@ -17,10 +17,10 @@ server.post('/api/users', async (req, res) => {
     try {
         if (!req.body.name || !req.body.bio) {
             res.status(400).json({
-                message: 'name and bio are required'
+                message: 'Please provide name and bio for the user'
             })
         } else {
-            const newUser = await User.create(req.body)
+            const newUser = await User.insert(req.body)
             res.status(201).json(newUser)
         }
     }catch (err) {
@@ -38,7 +38,7 @@ server.get("/api/users", async (req, res) => {
         res.json(users)
     }catch (err) {
         res.status(500).json({
-            message: 'error getting all users',
+            message: 'The users information could not be retrieved',
             error: err.message
         })
     }
@@ -50,16 +50,63 @@ server.get("/api/users/:id", async (req, res) => {
         const user = await User.findById(req.params.id)
         if(!user) {
             res.status(404).json({
-                message: `User by id ${req.params.id} does not exist`
+                message: `The user with the specified ID does not exist`
             })
         } else {
             res.json(user)
         }
     }catch (err) {
         res.status(500).json({
-            message: 'error getting all users',
+            message: 'The user information could not be retrieved',
             error: err.message
         })
     }
 })
+
+// [DELETE] /api/users/:id (D of CRUD, remove user with :id)
+server.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    User.remove(id)
+        .then(deletedUser => {
+            if (!deletedUser) {
+                res.status(404).json({
+                    message: `The user with the specified ID does not exist`
+                })
+            } else {
+            res.json(deletedUser)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The user could not be removed",
+                error: err.message
+            })
+        })
+})
+
+ // [PUT] /api/users/:id (U of CRUD, update user with :id using JSON payload)
+server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    const { body } = req
+    try {
+        const updated = await User.update(id, body)
+        if(!updated) {
+            res.status(404).json({
+                message: `The user with the specified ID does not exist`
+            })
+        } else if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: 'Please provide name and bio for the user'
+            })
+        } else {
+            res.status(200).json(updated)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The user information could not be modified',
+            error: err.message
+        })
+    }
+})
+
 module.exports = server; 
